3c73f37b8a590e66207219617a682877cb8dd3f4
Giovanni Pace <g.pace@braintechinnovations.it>
2026-02-16 17:01:49 +0100

    Aggiunto fino a HC

---
+++ lez01_containerbase/src/main/java/dev/archety/introduzione/Lez01ContainerbaseApplication.java
@@ -0,0 +1,13 @@
+package dev.archety.introduzione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lez01ContainerbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lez01ContainerbaseApplication.class, args);
+	}
+
+}
